0e803f5199200ae1e07d08dd36780798bce2e2d5
Austin King <shout@ozten.com>
2013-08-09 17:31:21 -0700

    Checkpointing, need to write notable updates to incremental JSON file

--- src/arewepopular.rs
+++ src/arewepopular.rs
@@ -32,55 +32,42 @@ fn main() {
     let mut web_repos:~[~str] = ~[];
 
     let mut idp_repos:~[~str] = ~[];
-/*
+
     analize(~"baseline",
             "function", counts);
     sleep(&uv::global_loop::get(), 3100);
-*/
-
-
-
 
     let (web_next_link, web_repos1) =
         analize(~"websites",
-            //navigator.id.get OR navigator.id.request
-
             // Testing 1 page Gigantopithecus AND blacki
             // Testing 3 pages Gigantopithecus AND bigfoot
-
-                "Gigantopithecus AND bigfoot", counts);
-    debug!("next link is %s and we've already got %?", web_next_link, web_repos1);
+                "navigator.id.get OR navigator.id.request", counts);
     web_repos.push_all_move(web_repos1);
     sleep(&uv::global_loop::get(), 3100);
 
-
-
-/*
-    let (idp_next_link, idp_repos) =
+    let (idp_next_link, idp_repos1) =
         analize(~"idproviders",
                 "navigator.id.beginProvisioning or navigator.id.genKeyPair", counts);
+    idp_repos.push_all_move(idp_repos1);
     sleep(&uv::global_loop::get(), 3100);
 
     analize(~"facebook",
             "//connect.facebook.net/en_US/all.js", counts);
 
     storage::update(today(), counts);
-    */
 
     // Adopters and Defectors
     /*
     1) Load yesterday's repo list
 */
     let yesterday_websites = load_repositories(~"websites");
+    let yesterday_idps = load_repositories(~"websites");
 
 /*
-    2) Iterate all of our results
+    2) Iterate all of our website results
     */
     let mut web_next_link2 = web_next_link;
-
-    if (web_next_link2.len() == 0) {
-        println("Only 1 page of results");
-    } else {
+    if (web_next_link2.len() != 0) {
         loop {
             let (count, repos, next_link) = get_search(web_next_link2.replace("https://api.github.com", "http://localhost:8002"));
             if (next_link.len() == 0) {
@@ -92,19 +79,15 @@ fn main() {
             web_repos.push_all_move(repos);
             sleep(&uv::global_loop::get(), 3100);
         }
-        println("Finished paginating results");
-        debug!(web_repos);
     }
-    let mut today_results = HashSet::new();
+    let mut today_web_results = HashSet::new();
     for web_repos.iter().advance |r| {
-        today_results.insert(r.clone());
+        today_web_results.insert(r.clone());
     }
-
-
     let mut notable:@mut HashMap<~str, ~HashMap<~str, ~[~str]>> = @mut HashMap::new();
     let mut notable_websites:~HashMap<~str, ~[~str]> = ~HashMap::new();
     let mut new_websites = ~[];
-    yesterday_websites.difference(&today_results, |diff| {
+    yesterday_websites.difference(&today_web_results, |diff| {
         debug!("Adopters yesterday_websites %?", diff);
         new_websites.push(diff.clone());
         true
@@ -112,19 +95,60 @@ fn main() {
     notable_websites.insert(~"adopters", new_websites);
 
     let mut missing_websites = ~[];
-    today_results.difference(&yesterday_websites, |diff| {
-        debug!("Defectors today_results %?", diff);
+    today_web_results.difference(&yesterday_websites, |diff| {
+        debug!("Defectors today_web_results %?", diff);
         missing_websites.push(diff.clone());
         true
     });
     notable_websites.insert(~"defectors", missing_websites);
 
     notable.insert(~"websites", notable_websites);
+
+    //2) Iterate all of our idp results
+//aok
+    let mut idp_next_link2 = idp_next_link;
+    if (idp_next_link2.len() != 0) {
+        loop {
+            let (count, repos, next_link) = get_search(idp_next_link2.replace("https://api.github.com", "http://localhost:8002"));
+            if (next_link.len() == 0) {
+                break;
+            }
+            idp_next_link2 = next_link.clone();
+
+            idp_repos.push_all_move(repos);
+            sleep(&uv::global_loop::get(), 3100);
+        }
+    }
+    let mut today_idp_results = HashSet::new();
+    for idp_repos.iter().advance |r| {
+        today_idp_results.insert(r.clone());
+    }
+    let mut notable:@mut HashMap<~str, ~HashMap<~str, ~[~str]>> = @mut HashMap::new();
+    let mut notable_idps:~HashMap<~str, ~[~str]> = ~HashMap::new();
+    let mut new_idps = ~[];
+    yesterday_idps.difference(&today_idp_results, |diff| {
+        debug!("Adopters yesterday_idps %?", diff);
+        new_idps.push(diff.clone());
+        true
+    });
+    notable_idps.insert(~"adopters", new_idps);
+
+    let mut missing_idps = ~[];
+    today_idp_results.difference(&yesterday_idps, |diff| {
+        debug!("Defectors today_idp_results %?", diff);
+        missing_idps.push(diff.clone());
+        true
+    });
+    notable_idps.insert(~"defectors", missing_idps);
+
+    notable.insert(~"ipds", notable_idps);
+    //aok
     storage::update_notable(today(), notable);
 
 
 /*
     5) Store lists
 */
-    //save_repositories(~"websites", web_repos);
+    save_repositories(~"websites", web_repos);
+    save_repositories(~"idps", idp_repos);
 }
