551d72e6effc43db4bf4b7fada63a48d0d7e1585
Austin King <shout@ozten.com>
2013-08-11 13:42:31 -0700

    Reverting last change

--- www/js/arewepopularyet.js
+++ www/js/arewepopularyet.js
@@ -1,45 +1,3 @@
-// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
-if (!Object.keys) {
-  Object.keys = (function () {
-    'use strict';
-    var hasOwnProperty = Object.prototype.hasOwnProperty,
-        hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
-        dontEnums = [
-          'toString',
-          'toLocaleString',
-          'valueOf',
-          'hasOwnProperty',
-          'isPrototypeOf',
-          'propertyIsEnumerable',
-          'constructor'
-        ],
-        dontEnumsLength = dontEnums.length;
-
-    return function (obj) {
-      if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
-        throw new TypeError('Object.keys called on non-object');
-      }
-
-      var result = [], prop, i;
-
-      for (prop in obj) {
-        if (hasOwnProperty.call(obj, prop)) {
-          result.push(prop);
-        }
-      }
-
-      if (hasDontEnumBug) {
-        for (i = 0; i < dontEnumsLength; i++) {
-          if (hasOwnProperty.call(obj, dontEnums[i])) {
-            result.push(dontEnums[i]);
-          }
-        }
-      }
-      return result;
-    };
-  }());
-}
-
 function layoutAndStyle() {
   var h = $(window).height();
 
@@ -74,7 +32,7 @@ $.getJSON('/data/daily_counts.json', function(data, status, jqxhr) {
         var key = keys[i];
         dates.push(Date.parse(key));
     }
-    dates = Array.prototype.sort(dates).slice(-6);
+    dates = Array.sort(dates).slice(-6);
 
     var dataPoints = [[],[],[], []];
     for (var i=0; i < dates.length; i++) {
